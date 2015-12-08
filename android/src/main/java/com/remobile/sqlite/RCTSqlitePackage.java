81a4f673dec9247ea804beaa90d02ff85481da28
fov42550564 <42550564@qq.com>
2015-12-08 09:23:44 +0800

    init commit

---
+++ android/src/main/java/com/remobile/sqlite/RCTSqlitePackage.java
@@ -0,0 +1,51 @@
+package com.remobile.sqlite;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Intent;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+
+public class RCTSqlitePackage implements ReactPackage {
+
+    private Activity activity;
+    private SQLitePlugin mModuleInstance;
+
+    public RCTSqlitePackage(Activity activity) {
+        super();
+        this.activity = activity;
+    }
+
+
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+        mModuleInstance = new SQLitePlugin(reactContext, activity);
+        return Arrays.<NativeModule>asList(
+                mModuleInstance
+        );
+    }
+
+    @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Arrays.<ViewManager>asList();
+    }
+
+    public void onDestroy() {
+        if (mModuleInstance != null) {
+            mModuleInstance.onDestroy();
+        }
+    }
+}
