83595d80bd4fbcc254834db0bc4212b27adb3a54
fov42550564 <42550564@qq.com>
2016-10-11 14:20:53 +0800

    升级到1.0.2

--- android/src/main/java/com/remobile/sqlite/RCTSqlitePackage.java
+++ android/src/main/java/com/remobile/sqlite/RCTSqlitePackage.java
@@ -4,32 +4,17 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import android.app.Activity;
-import android.content.Intent;
-
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
-
 public class RCTSqlitePackage implements ReactPackage {
-
-    private Activity activity;
-    private SQLitePlugin mModuleInstance;
-
-    public RCTSqlitePackage(Activity activity) {
-        super();
-        this.activity = activity;
-    }
-
-
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-        mModuleInstance = new SQLitePlugin(reactContext, activity);
         return Arrays.<NativeModule>asList(
-                mModuleInstance
+                new SQLitePlugin(reactContext)
         );
     }
 
@@ -41,11 +26,5 @@ public class RCTSqlitePackage implements ReactPackage {
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Arrays.<ViewManager>asList();
-    }
-
-    public void onDestroy() {
-        if (mModuleInstance != null) {
-            mModuleInstance.onDestroy();
-        }
     }
 }
